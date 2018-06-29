332525ffb399934cd6410f48472639bfc557a002
Matt Peterson <ricochet1k@gmail.com>
2018-06-29 13:26:17 -0400

    Much more stuff in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@
 
 cd $(dirname "$0")
 
-mkdir -p external
-if [ ! -e bork ] ; then
-  (cd external; git clone https://github.com/mattly/bork)
-fi
+[ -d external ] || mkdir -p external
+[ -d external/bork ] || (cd external; git clone https://github.com/mattly/bork)
+
+type rustup || curl https://sh.rustup.rs -sSf | sh
+
+type fd || cargo install fd-find
+type exa || cargo install exa
+type rg || cargo install ripgrep
+
+type kak-lsp || cargo install --force --git https://github.com/ul/kak-lsp
 
